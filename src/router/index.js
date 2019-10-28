b03ef0b3447c342a45d2796dd137bcb1f1d260fb
brittani <britt.sentiere@gmail.com>
2019-10-28 16:20:43 -0500

    added signup, login, and logout vues

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Signup from '../views/Signup.vue'
+import Login from '../views/Login.vue'
+import Logout from '../views/Logout.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  
+   {path: "/signup", name: "signup", component: Signup },
+   {path: "/login", name: "login", component: Login },
+   {path: "/logout", name: "logout", component: Logout }
+
+ 
 ]
 
 const router = new VueRouter({
@@ -27,3 +23,5 @@ const router = new VueRouter({
 })
 
 export default router
+
+
