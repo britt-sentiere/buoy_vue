e7fedce09c20053785257ff0b9f69f124d1c7715
brittani <britt.sentiere@gmail.com>
2019-11-12 16:49:26 -0600

    removed old login data on login.vue

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -60,36 +60,6 @@
             </div>
         </div>
     </div>
-    <!-- Login area end -->
-    <!-- <div class="container">
-      <form v-on:submit.prevent="submit()">
-        <h1>Signup</h1>
-        <ul>
-          <li class="text-danger" v-for="error in errors">{{ error }}</li>
-        </ul>
-        <div class="form-group">
-          <label>First Name:</label>
-          <input type="text" class="form-control" v-model="firstName">
-        </div>
-        <div class="form-group">
-          <label>Last Name:</label>
-          <input type="text" class="form-control" v-model="lastName">
-        </div>
-        <div class="form-group">
-          <label>Email:</label>
-          <input type="text" class="form-control" v-model="email">
-        </div>
-        <div class="form-group">
-          <label>Password:</label>
-          <input type="password" class="form-control" v-model="password">
-        </div>
-        <div class="form-group">
-          <label>Password Confirmation:</label>
-          <input type="password" class="form-control" v-model="passwordConfirmation">
-        </div>
-        <input type="submit" class="btn btn-primary" value="Submit">
-      </form>
-    </div> -->
   </div>
 </template>
 
